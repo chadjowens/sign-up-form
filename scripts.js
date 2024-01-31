07fef0d4e792f091c22ed33d4c07a82b9df261df
chadjowens <chadjowens@gmail.com>
2024-01-31 12:45:38 -0600

    small layout tweaks, updates to text and styling.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-window.onload = function() {
-    document.body.innerHTML += "<p>Hello World</p>";
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+const errorMessage = document.querySelector(".errorMessage");
+
+password.addEventListener("input", checkPasswordMatch);
+confirmPassword.addEventListener("input", checkPasswordMatch);
+
+function checkPasswordMatch() {
+  console.log(password.value);
+  console.log(confirmPassword.value);
+  if (password.value !== confirmPassword.value) {
+    password.className = "error";
+    confirmPassword.className = "error";
+    errorMessage.className = "errorMessage showErrorMessage";
+  } else {
+    password.className = "";
+    confirmPassword.className = "";
+    errorMessage.className = "errorMessage";
+  }
 }
